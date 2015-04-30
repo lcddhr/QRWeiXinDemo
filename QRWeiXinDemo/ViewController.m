5b8e650d0a26138220174d77939f27c2541be13d
liuchendi <lcddhr@qq.com>
2015-04-30 18:58:27 +0800

    增加摄像头有效性判断
    增加摄像头有效性判断

--- QRWeiXinDemo/ViewController.m
+++ QRWeiXinDemo/ViewController.m
@@ -21,6 +21,30 @@
     // Do any additional setup after loading the view, typically from a nib.
     
 }
+- (IBAction)openQR:(id)sender {
+    
+    if ([self validateCamera]) {
+        
+        [self showQRViewController];
+        
+    } else {
+        
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"提示" message:@"没有摄像头或摄像头不可用" delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
+        [alertView show];
+    }
+}
+
+- (BOOL)validateCamera {
+    
+  return [UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera] &&
+         [UIImagePickerController isCameraDeviceAvailable:UIImagePickerControllerCameraDeviceRear];
+}
+
+- (void)showQRViewController {
+    
+    QRViewController *qrVC = [[QRViewController alloc] init];
+    [self.navigationController pushViewController:qrVC animated:YES];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
